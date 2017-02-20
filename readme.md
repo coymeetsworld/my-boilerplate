960d138a56b7da179a9fffc19e3258ce80acfa8b
Coy Sanders <coymeetsworld@gmail.com>
2017-02-20 01:44:17 -0800

    Initial commit

---
+++ readme.md
@@ -0,0 +1,6 @@
+
+This is my new version of boilerplate code that now uses Webpack 2. Has the following included:
+
+normalize.scss
+react/redux
+
---
+++ src/app.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => (
+	<div className="main-app">
+		<h1>Boilerplate Project</h1>
+	</div>
+)
+
+export default App;
